8c80323508e8f324f356b8f12b5bb9764373b823
EEtancelin <etancelin.edouard@gmail.com>
2016-10-18 09:40:49 +0200

    Create the fines and offer controller and the route.

---
+++ app/controllers/offer_controller.rb
@@ -0,0 +1,13 @@
+class OfferController < ApplicationController
+  def index
+  end
+
+  def update
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  ressources :offers, only: [:index,:new, :create, :update] do
+    ressources : :fines , only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  end
+
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
