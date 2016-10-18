9f717941a435e5bb63b8b6904970254dd2efcd4b
EEtancelin <etancelin.edouard@gmail.com>
2016-10-18 17:15:12 +0200

    Fix fine and user model and the seed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :offer, dependent: :destroy
-  belongs_to :fine, dependent: :destroy
+  has_many :offers, dependent: :destroy
+  has_many :fines, dependent: :destroy
 end
