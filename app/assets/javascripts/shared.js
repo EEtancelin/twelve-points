b2290168d83ac01cac9fb3586636db6892ec9bd4
EEtancelin <etancelin.edouard@gmail.com>
2016-10-19 13:01:29 +0200

    Implement the tab selection.

--- app/assets/javascripts/shared.js
+++ app/assets/javascripts/shared.js
@@ -1,19 +1,31 @@
 
 
-// This  Jquery listen  elment with the redirect-to class.
+// This  Jquery listen  element with the redirect-to class.
 //Read the href attribute of this class and redirect to this element.
+
+var displayTabContent = function(tab){};
+
+var resetTabs = function(){
+  $(".tab-content").hide();
+  $(".tab").removeClass("active");
+};
+
+var displayselectedTab = function(selected_tab){
+  $(selected_tab).addClass("active");
+  selected_content = selected_tab.attr('content-id');
+  $(selected_content).show();
+};
+
+
 $('.redirect-to').click(function(event) {
     var clicked_element = $(this);
     window.location.href = clicked_element.attr('href');
 });
 
 
-$(".tabs").tabs({
-    activate: function (event, ui) {
-        var active = $('.tabs').tabs('option', 'active');
-        $(".tabid").html('the tab id is ' + $("#tabs ul>li a").eq(active).attr("href"));
-
-    }
-}
-
-  );
+//Function to display
+$(".tab").click(function(event){
+  selected_tab = $(this);
+  resetTabs();
+  displayselectedTab(selected_tab);
+});
