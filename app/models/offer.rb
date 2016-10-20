d4b3d9fb23b971ca9ae99aa1f2c569b743329aba
EEtancelin <etancelin.edouard@gmail.com>
2016-10-20 15:58:11 +0200

    add validation to model offer

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,11 @@
 class Offer < ApplicationRecord
   belongs_to :user
   belongs_to :fine
+
+  validates :deadline, presence: true
+  validates :user_id, presence: true
+  validates :fine_id, presence: true
+
+  end
+
 end
