15bba8ea3e37bb314f2e5939366924ab448e3c1f
Arnaud COUESNON <arnaud.couesnon@gmail.com>
2016-10-21 16:15:16 +0200

    mailing

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :fines, dependent: :destroy
 
   mount_uploader :photo, PhotoUploader
-  after_create :send_welcome_email
+  # after_create :send_welcome_email
 
   def self.find_for_facebook_oauth(auth)
     user_params = auth.to_h.slice(:provider, :uid)
